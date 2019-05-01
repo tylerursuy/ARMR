7aff563adf0c9fabe19e09e83d4fe3444a0e4263
Nicole Kacirek <nicolekacirek@Nicoles-MacBook-Pro.local>
2019-04-30 20:20:03 -0700

    basic jobs example

--- code/config.py
+++ code/config.py
@@ -5,3 +5,19 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = f"postgresql://{user}:\
 {pw}@armr.c4eooxhj8ss8.us-west-1.rds.amazonaws.com:5432/armr"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    # APSchedule
+    JOBS = [
+        {
+            'id': 'job1',
+            'func': 'config:job1',
+            'args': (1, 2),
+            'trigger': 'interval',
+            'seconds': 10
+        }
+    ]
+    SCHEDULER_API_ENABLED = True
+
+
+def job1(a, b):
+    print(str(a) + ' ' + str(b))
